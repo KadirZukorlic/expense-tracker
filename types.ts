519e4a86f9a3b2c01a431c8f92b9a2ac4c794e9c
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-01-13 16:03:42 +0100

    Fix Typescript error "Doesnt satisfy constraints "

--- types.ts
+++ types.ts
@@ -1,6 +1,13 @@
-export interface ExpenseItemProps {
+export type ExpenseItemProps = {
 	id: string
 	description: string
 	amount: number
 	date: Date
+}
+
+export type RootStackParamList = {
+	AllExpenses: undefined
+	RecentExpenses: undefined
+	ExpensesOverview: undefined
+	ManageExpense: undefined
 }
