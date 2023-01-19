b8b089a1b461acffbde74ce5f02848d86e1a466a
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-01-19 18:59:28 +0100

    Showing recent expenses

--- components/ExpensesOutput/ExpensesOutput.tsx
+++ components/ExpensesOutput/ExpensesOutput.tsx
@@ -8,62 +8,11 @@ interface ExpensesOutputProps {
 	expensesPeriod: string
 }
 
-const DUMMY_EXPENSES = [
-	{
-		id: 'e1',
-		description: 'A pair of shoes',
-		amount: 59.99,
-		date: new Date('2023-01-10')
-	},
-	{
-		id: 'e2',
-		description: 'A pair of trousers',
-		amount: 89.29,
-		date: new Date('2022-01-15')
-	},
-	{
-		id: 'e3',
-		description: 'Some bananas',
-		amount: 5.99,
-		date: new Date('2022-12-05')
-	},
-	{
-		id: 'e4',
-		description: 'A Book',
-		amount: 14.99,
-		date: new Date('2022-12-31')
-	},
-	{
-		id: 'e5',
-		description: 'Another Book',
-		amount: 19.99,
-		date: new Date('2023-01-09')
-	},
-	{
-		id: 'e6',
-		description: 'A pair of trousers',
-		amount: 89.29,
-		date: new Date('2022-01-15')
-	},
-	{
-		id: 'e7',
-		description: 'A Book',
-		amount: 14.99,
-		date: new Date('2022-12-31')
-	},
-	{
-		id: 'e8',
-		description: 'Another Book',
-		amount: 19.99,
-		date: new Date('2023-01-09')
-	}
-]
-
 const ExpensesOutput = ({ expenses, expensesPeriod }: ExpensesOutputProps) => {
 	return (
 		<View style={styles.container}>
-			<ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-			<ExpensesList expenses={DUMMY_EXPENSES} />
+			<ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+			<ExpensesList expenses={expenses} />
 		</View>
 	)
 }
--- util/date.ts
+++ util/date.ts
@@ -1,3 +1,7 @@
 export function getFormattedDate(date: Date) {
 	return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDay()}`
 }
+
+export function getDateMinusDays(date: Date, days: number) {
+	return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days)
+}
